692e0e06a9f79556cec2f1de96e7301f0ea164d3
BURAK OZDEMIR <ozdemirburak1912@gmail.com>
2022-06-02 16:44:07 +0300

    edit

--- src/components/Skills.jsx
+++ src/components/Skills.jsx
@@ -7,7 +7,6 @@ import ReactImg from '../assets/react.png';
 import Node from '../assets/node.png';
 import FireBase from '../assets/firebase.png';
 import GitHub from '../assets/github.png';
-import Tailwind from '../assets/tailwind.png';
 import Mongo from '../assets/mongo.png';
 
 const Skills = () => {
